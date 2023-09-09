25571505121c426eb23d3aae1b73e498b112877c
JennyMa0517 <mjh19960517@icloud.com>
2023-09-09 13:24:13 +0800

    chore: complete read data

--- personalized_classification/utils.py
+++ personalized_classification/utils.py
@@ -1,3 +1,4 @@
+import csv
 from os import walk, path
 from typing import List
 
@@ -10,28 +11,71 @@ class Common:
     n_classes = len(classes)
     count_classes = [0] * n_classes
 
-    x: List[str] = []
-    y: List[str] = []
-
-    records: List[str] = []
-    annotations: List[str] = []
-
     def __init__(self, data_path: str):
         self.data_path = data_path
 
-    def read_files(self) -> None:
+    def read_data(self) -> List[List[int]]:
+        x: List[List[int]] = []
+        y: List[int] = []
+
+        records: List[str] = []
+        annotations: List[str] = []
+
         all_file_paths: List[str] = []
         for dirpath, dirnames, filenames in walk(self.data_path):
             for filename in filenames:
                 full_path = path.join(dirpath, filename)
                 all_file_paths.append(full_path)
+        all_file_paths.sort()
 
         for file_path in all_file_paths:
             filename, file_extension = path.splitext(file_path)
             if file_extension == ".txt":
-                self.annotations.append(file_path)
+                annotations.append(file_path)
             elif file_extension == ".csv":
-                self.records.append(file_path)
+                records.append(file_path)
 
-        for record in self.records:
-            sigals = []
+        for (record, annotation) in zip(records, annotations):
+            signals: List[int] = []
+            with open(record, "r") as csvfile:
+                reader = csv.reader(
+                    csvfile,
+                    quotechar="|",
+                    delimiter=",",
+                )
+                next(reader)  # skip header
+
+                for row in reader:
+                    signals.append(int(row[1]))
+
+            beat = []
+            with open(annotation, "r") as txtfile:
+                content = txtfile.readlines()
+                for index in range(1, len(content)):
+                    splitted = list(
+                        filter(lambda v: v != '',
+                               content[index].strip().split(' ')))
+                    pos = int(splitted[1])
+                    arrhythmia_type = splitted[2]
+
+                    if (arrhythmia_type in self.classes):
+                        arrhythmia_index = self.classes.index(arrhythmia_type)
+                        # avoid overfitting
+                        if self.count_classes[
+                                arrhythmia_index] > self.maximum_counting:
+                            pass
+                        else:
+                            self.count_classes[arrhythmia_index] += 1
+                            if self.window_size < pos < (len(signals) -
+                                                         self.window_size):
+                                beat = signals[pos - self.window_size + 1:pos +
+                                               self.window_size]
+                                x.append(beat)
+                                y.append(arrhythmia_index)
+
+        data: List[List[int]] = []
+        for (i, v) in enumerate(x):
+            data.append(v)
+            data[i].append(y[i])
+
+        return x
--- scripts/example.py
+++ scripts/example.py
@@ -3,6 +3,5 @@ from personalized_classification.utils import Common
 
 data_path = path.join(path.dirname(__file__), "../", "data")
 
-
 tool = Common(data_path)
-tool.read_files()
+data = tool.read_data()
